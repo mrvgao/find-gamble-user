b3bb0308101318c8bf68b084b445d52b529ca37c
i-gaominquan <>
2017-11-21 18:00:18 +0800

    rewrite get vocab size, skip the first line

--- get_vocab_size.py
+++ get_vocab_size.py
@@ -1,12 +1,14 @@
 all_char = {}
 
 with open('database/train_corpus.tsv', encoding='utf-8') as f:
-    for line in f:
+    for ii, line in enumerate(f):
+        if ii == 0: continue
         char = line.split('\t')[0]
         all_char[char] = True
 
 
-print('vocab_size: {}'.format(len(all_char)))
+vocab_size = len(all_char)
+print('vocab_size: {}'.format(vocab_size))
 
 with open('database/vocab_size.txt', 'w') as f:
-    f.write('vocab_size:{}'.format(len(all_char)))
+    f.write('vocab_size:{}'.format(vocab_size))
